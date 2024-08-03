0ae8af83275fb9c6ca6d2b212f7dd115409a7a10
muhammeteminbaytar <muhammet2000byt@gmail.com>
2024-08-03 12:32:46 +0300

    added data binding

--- app/src/main/java/com/example/emarketcaseapp/presentation/view/MainActivity.kt
+++ app/src/main/java/com/example/emarketcaseapp/presentation/view/MainActivity.kt
@@ -7,17 +7,21 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.emarketcaseapp.R
+import com.example.emarketcaseapp.databinding.ActivityMainBinding
 import com.example.emarketcaseapp.presentation.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private val viewModel: MainViewModel by viewModels()
+    private lateinit var activityMainBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
