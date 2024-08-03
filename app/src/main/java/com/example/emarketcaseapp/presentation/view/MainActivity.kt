2d9d0a42a2775500eb64af8e1bde0ed5197f227c
muhammeteminbaytar <muhammet2000byt@gmail.com>
2024-08-03 12:51:46 +0300

    connect fragment container and bottom bar.

--- app/src/main/java/com/example/emarketcaseapp/presentation/view/MainActivity.kt
+++ app/src/main/java/com/example/emarketcaseapp/presentation/view/MainActivity.kt
@@ -6,10 +6,13 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.emarketcaseapp.R
 import com.example.emarketcaseapp.databinding.ActivityMainBinding
 import com.example.emarketcaseapp.presentation.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -19,7 +22,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
         activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(activityMainBinding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -27,5 +29,11 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(activityMainBinding.navHostFragment.id) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        activityMainBinding.bottomBar.setupWithNavController(navController)
     }
 }
